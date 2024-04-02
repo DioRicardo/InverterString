06b918f477eb85cc0695179edd4c71ddbb839cd3
Dio Ricardo <dio.vieira@live.com>
2024-04-02 14:25:28 -0300

    Subindo o Projeto

---
+++ InverterString/Modelos/InversaoDeString.cs
@@ -0,0 +1,16 @@
+﻿namespace InverterString.Modelos;
+
+internal class InversaoDeString
+{
+    public string InverseString { get; set; } = "";
+
+    public string InvertString(string str)
+    {
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            InverseString += str[i];
+        }
+
+        return InverseString;
+    }
+}
---
+++ InverterString/Program.cs
@@ -0,0 +1,11 @@
+﻿using InverterString.Modelos;
+
+InversaoDeString inverter = new();
+
+Console.WriteLine("Digite uma string para ser invertida: ");
+
+string inputString = Console.ReadLine()!;
+
+string stringInvertida = inverter.InvertString(inputString);
+
+Console.WriteLine(stringInvertida);
